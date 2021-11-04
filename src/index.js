c817bace897be935280f0a3b643a21df00d990bb
ghost-tremble <77670549+ghost-tremble@users.noreply.github.com>
2021-11-04 20:18:40 +0100

    boiler plate code changes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./Components/App"
+
+
+
+
+
+
+
+
+
+
+
+
+const Renderable = document.getElementById("root");
+ReactDOM.render(<App/>,Renderable)
