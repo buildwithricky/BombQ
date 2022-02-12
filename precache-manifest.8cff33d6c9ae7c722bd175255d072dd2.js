45caca03307b1e47aaa440dc1593619a60fc7aed
ghost-tremble <patokezy@gmail.com>
2022-02-12 14:46:28 +0100

    Updates

---
+++ precache-manifest.8cff33d6c9ae7c722bd175255d072dd2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ee213c386eb6924ed6c10ec1a41768",
+    "url": "/BombQ%20/index.html"
+  },
+  {
+    "revision": "675b5f83f71eba696f58",
+    "url": "/BombQ%20/static/css/3.b5004b8f.chunk.css"
+  },
+  {
+    "revision": "5adc7bcd976d59b57be6",
+    "url": "/BombQ%20/static/css/main.efd4786e.chunk.css"
+  },
+  {
+    "revision": "fc398be021abc1ecaceb",
+    "url": "/BombQ%20/static/js/2.327cb7d5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/BombQ%20/static/js/2.327cb7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675b5f83f71eba696f58",
+    "url": "/BombQ%20/static/js/3.62df543d.chunk.js"
+  },
+  {
+    "revision": "5adc7bcd976d59b57be6",
+    "url": "/BombQ%20/static/js/main.5aebc2ea.chunk.js"
+  },
+  {
+    "revision": "0ad0d9c8c5f42b128ac1",
+    "url": "/BombQ%20/static/js/runtime-main.f5422d60.js"
+  },
+  {
+    "revision": "e103de44af1d7a95478c92e530ac6dff",
+    "url": "/BombQ%20/static/media/BombQ.e103de44.png"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/BombQ%20/static/media/shopping-bag.30f07071.svg"
+  }
+]);
