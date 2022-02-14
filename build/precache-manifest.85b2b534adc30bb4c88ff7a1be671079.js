e875ad44e302f3cb1f9cbe6398621d567fa907d1
ghost-tremble <patokezy@gmail.com>
2022-02-14 19:04:23 +0100

    changes made to checkout component

---
+++ build/precache-manifest.85b2b534adc30bb4c88ff7a1be671079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be98fac4bf146b7ce2f7305b593a1481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d57ef4acde15fee4bf78",
+    "url": "/static/css/3.b5004b8f.chunk.css"
+  },
+  {
+    "revision": "2e2beeca1c74197c80ff",
+    "url": "/static/css/main.d639e1c9.chunk.css"
+  },
+  {
+    "revision": "fc398be021abc1ecaceb",
+    "url": "/static/js/2.327cb7d5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.327cb7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57ef4acde15fee4bf78",
+    "url": "/static/js/3.79ae457f.chunk.js"
+  },
+  {
+    "revision": "2e2beeca1c74197c80ff",
+    "url": "/static/js/main.b9784a37.chunk.js"
+  },
+  {
+    "revision": "a05abad932fe754baf81",
+    "url": "/static/js/runtime-main.631774e7.js"
+  },
+  {
+    "revision": "e103de44af1d7a95478c92e530ac6dff",
+    "url": "/static/media/BombQ.e103de44.png"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
